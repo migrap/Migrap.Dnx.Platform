2c96a9c84920ea74dea9a930f5a64349f810a847
Michael Park <migrap@outlook.com>
2015-10-21 15:13:36 -0700

    Add missing files

---
+++ src/Migrap.Dnx.Platform/IPlatformEnvironment.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Dnx.Runtime;
+using System;
+
+namespace Migrap.Dnx.Platform {
+    public interface IPlatformEnvironment {
+        bool Is(Func<IPlatformEnvironmentExtension, Func<IRuntimeEnvironment, bool>> platform);
+        bool Any(params Func<IPlatformEnvironmentExtension, Func<IRuntimeEnvironment, bool>>[] platforms);
+    }
+}
---
+++ src/Migrap.Dnx.Platform/PlatformEnvironment.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Dnx.Runtime;
+using System;
+using System.Linq;
+
+namespace Migrap.Dnx.Platform {
+    public partial class PlatformEnvironment : IPlatformEnvironment {
+        private readonly IRuntimeEnvironment _runtime;
+
+        public PlatformEnvironment(IRuntimeEnvironment runtime) {
+            _runtime = runtime;
+        }
+
+        public bool Is(Func<IPlatformEnvironmentExtension, Func<IRuntimeEnvironment, bool>> platform) {
+            return platform(null)(_runtime);
+        }
+
+        public bool Any(params Func<IPlatformEnvironmentExtension, Func<IRuntimeEnvironment, bool>>[] platforms) {
+            return platforms.Any(x => x(null)(_runtime));
+        }      
+    }
+}
